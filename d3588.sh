ffcfb58fc046c698f8efbbf3c96c488be7cad9b9
yifengyou <842056007@qq.com>
2025-04-05 13:48:14 +0800

    add d3588 support
    Signed-off-by: yifengyou <842056007@qq.com>

---
+++ d3588.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+JOB=`sed -n "N;/processor/p" /proc/cpuinfo|wc -l`
+
+# CROSS_COMPILE_ARM64=aarch64-none-linux-gnu-
+GCC=`realpath ../gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu`
+CROSS_COMPILE_ARM64=${GCC}/bin/aarch64-none-linux-gnu-
+echo "using gcc: [${CROSS_COMPILE_ARM64}]"
+
+export KERNEL_TARGET=d3588
+#export RK_KERNEL_DTB=rk-kernel.dtb
+RK_KERNEL_DEFCONFIG_FRAGMENT=
+
+# kernel
+make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE_ARM64 ${KERNEL_TARGET}_defconfig
+make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE_ARM64 -j$JOB
+# make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE_ARM64 dtbs -j$JOB
+# make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE_ARM64 ${KERNEL_TARGET}.img
+
+mkdir -p ../tools/
+cp arch/arm64/boot/Image ../tools/
+
+echo "All done! [$?]"
+
